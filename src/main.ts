aeaf73bc389137d4e9a7bc436b098bdb80deaf38
raja <rajababuchauhan500@gmail.com>
2025-05-06 10:35:06 +0200

    cleaned the content

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,6 @@
-// import { Home } from "./rlib/timeline/t2025/april/DomainOps/Home";
 import { Page } from "./rlib/timeline/t2025/may/ToolsHomepage/Page";
 import "./style.css";
-import { StructureForm } from "./rlib/timeline/t2025/april/DomainOps/StructureForm";
-// const home = new Home();
-const page = Page();
-// const cf = StructureForm();
 
-// document
-//     .querySelector<HTMLDivElement>("#app")!
-//     .appendChild(cf.crud.layout.getElement());
+const page = Page();
 
 document.querySelector<HTMLDivElement>("#app")!.appendChild(page.getElement());
