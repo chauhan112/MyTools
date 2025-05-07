956f26816f0d0a7aa21268f948f41ace92246a09
raja <rajababuchauhan500@gmail.com>
2025-05-07 22:33:32 +0200

    added logo image

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,9 @@ import { Page } from "./rlib/timeline/t2025/may/ToolsHomepage/Page";
 import "./style.css";
 
 const page = Page();
+page.s.wrapper.s.header.s.wrapper.s.goBack.s.img.update({
+    src: "logo.png",
+});
+console.log();
 
 document.querySelector<HTMLDivElement>("#app")!.appendChild(page.getElement());
