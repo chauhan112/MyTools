01d00ac609d38d3601cee0f29a1f0adf5e284d21
raja <rajababuchauhan500@gmail.com>
2025-05-10 15:17:53 +0200

    adding an app

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { Page } from "./rlib/timeline/t2025/may/ToolsHomepage/Page";
 import "./style.css";
-
+import { CircleCheck } from "lucide";
 const page = Page();
-page.s.wrapper.s.header.s.wrapper.s.goBack.s.img.update({
+page.layout.s.wrapper.s.header.s.wrapper.s.goBack.s.img.update({
     src: "logo.png",
 });
-console.log();
-
+page.router.route();
+page.addApp({
+    title: "Task Manager",
+    description: "Organize your daily tasks and boost productivity.",
+    link: "task-manager/",
+    routeFunc: () => {
+        page.mainBody.clear();
+    },
+    icon: CircleCheck,
+});
+page.router.route();
+// CardComponent("Task Manager");
 document.querySelector<HTMLDivElement>("#app")!.appendChild(page.getElement());
