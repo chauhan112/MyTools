889a1dfd4b884e0a402258a635836be3c69b543e
raja <rajababuchauhan500@gmail.com>
2025-05-17 00:16:15 +0200

    added vite config for ace-editor

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
     plugins: [
@@ -12,6 +13,14 @@ export default defineConfig({
                 process: true,
             },
             protocolImports: true,
+        }),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: "node_modules/ace-builds/src-noconflict",
+                    dest: "assets",
+                },
+            ],
         }),
     ],
 });
