dd6c455372483f48d122a4b476e0baaaf958b37c
raja <rajababuchauhan500@gmail.com>
2025-05-17 13:49:35 +0200

    cleaned main

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,6 @@ import { CircleCheck } from "lucide";
 import { Page as FileSearchPage } from "./rlib/timeline/t2025/may/FileSearch/Components";
 
 import { DEF_TITLE } from "./rlib/timeline/t2025/may/ToolsHomepage/Components";
-// import { Tools } from "./rlib/timeline/t2025/april/tools";
-// import { GComponent } from "./rlib/timeline/t2025/april/GComponent";
-
-// const cs = new ContentSearch();
 
 const page = Page();
 page.layout.s.wrapper.s.header.s.wrapper.s.goBack.s.img.update({
@@ -30,47 +26,6 @@ page.addApp({
 });
 
 page.addApp({
-    title: "Git Clone Test",
-    description: "Just Testing Git Clone",
-    link: "git-clone/",
-    routeFunc: () => {
-        page.mainBody.clear();
-        // page.mainBody.comp.update({
-        //     textContent: "Git Clone Test",
-        // });
-        // igw.clone("https://github.com/chauhan112/JSlib.git").then(() => {
-        //     console.log("Cloned");
-        // });
-        // igw.pull({ name: "chauhan112", email: "rajababuchauhan500@gmail.com" });
-        // fs.listfilesWithIgnore("/", [".git"]).then((res) => {
-        //     console.log(res);
-        // });
-        // fs.read("/repo/timeline/t2024/unblock.js").then((res) => {
-        //     let area = Tools.comp("textarea", {
-        //         placeholder: "Hello",
-        //         class: "flex-1",
-        //     });
-        //     area.getElement().value = res;
-        //     // area.update({ value: res });
-        //     page.mainBody.display(area);
-        // });
-        // fs.dirlist("/", false).then((dirs) => {
-        //     console.log(dirs);
-        //     dirs.forEach((dir) => {
-        //         fs.delete(dir, true).catch(console.error);
-        //     });
-        // });
-        // const inp = document.createElement("input");
-        // inp.addEventListener("onchange", (e: any) =>
-        //     console.log(e.target.value)
-        // );
-
-        // page.mainBody.comp.getElement().appendChild(inp);
-    },
-    icon: CircleCheck,
-});
-
-page.addApp({
     title: "Links Opener",
     description: "Search in your git repo files content",
     link: "task-manager/",
@@ -85,5 +40,4 @@ page.addApp({
     icon: CircleCheck,
 });
 page.router.route();
-// CardComponent("Task Manager");
 document.querySelector<HTMLDivElement>("#app")!.appendChild(page.getElement());
