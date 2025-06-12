fe3c79f90485441150804e0fd0d191b544b772a6
raja <rajababuchauhan500@gmail.com>
2025-06-12 16:07:38 +0200

    added  html to my lib tool

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { Page } from "./rlib/timeline/t2025/may/ToolsHomepage/Page";
 import "./style.css";
 import { CircleCheck } from "lucide";
 import { Page as FileSearchPage } from "./rlib/timeline/t2025/may/FileSearch/Components";
+import { Page as HtmlToMyLib } from "./rlib/timeline/t2025/may/HtmlToMyLib/index";
 
 import { DEF_TITLE } from "./rlib/timeline/t2025/may/ToolsHomepage/Components";
 
@@ -35,6 +36,21 @@ page.addApp({
         page.mainBody.display(fspage);
         page.layout.s.wrapper.s.header.s.updateTitle(
             DEF_TITLE + " - " + "Content Searching"
+        );
+    },
+    icon: CircleCheck,
+});
+
+page.addApp({
+    title: "HTML to my lib",
+    description: "convert html to js format",
+    link: "html-to-my-lib/",
+    routeFunc: () => {
+        const fspage = HtmlToMyLib();
+        page.mainBody.clear();
+        page.mainBody.display(fspage);
+        page.layout.s.wrapper.s.header.s.updateTitle(
+            DEF_TITLE + " - " + "HTML to my lib"
         );
     },
     icon: CircleCheck,
