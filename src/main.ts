48dde1928cf29d65e7e1c3c236e8c01fe1658ad1
raja <rajababuchauhan500@gmail.com>
2025-07-04 16:00:52 +0200

    code factoring and changes to the links opener

--- src/main.ts
+++ src/main.ts
@@ -1,57 +1,6 @@
-import { Page } from "./rlib/timeline/t2025/may/ToolsHomepage/Page";
 import "./style.css";
-import { CircleCheck } from "lucide";
-import { Page as FileSearchPage } from "./rlib/timeline/t2025/may/FileSearch/Components";
-import { Page as HtmlToMyLib } from "./rlib/timeline/t2025/may/HtmlToMyLib/index";
-import { Page as LinksOpener } from "./rlib/timeline/t2025/may/LinksOpener/Page";
+import { MainPage } from "./rlib/timeline/t2025/may/ToolsHomepage/Page";
 
-import { DEF_TITLE } from "./rlib/timeline/t2025/may/ToolsHomepage/Components";
+const page = MainPage();
 
-const page = Page();
-page.layout.s.wrapper.s.header.s.wrapper.s.goBack.s.img.update({
-    src: "logo.png",
-});
-page.addApp({
-    title: "Content Searching",
-    description: "Search in your git repo files content",
-    link: "task-manager/",
-    routeFunc: () => {
-        const fspage = FileSearchPage();
-        page.mainBody.clear();
-        page.mainBody.display(fspage);
-        page.layout.s.wrapper.s.header.s.updateTitle(
-            DEF_TITLE + " - " + "Content Searching"
-        );
-    },
-    icon: CircleCheck,
-});
-page.addApp({
-    title: "Links Opener",
-    description: "Search in your git repo files content",
-    link: "links-opener/",
-    routeFunc: () => {
-        const fspage = LinksOpener();
-        page.mainBody.clear();
-        page.mainBody.display(fspage);
-        page.layout.s.wrapper.s.header.s.updateTitle(
-            DEF_TITLE + " - " + "Links Opener"
-        );
-    },
-    icon: CircleCheck,
-});
-page.addApp({
-    title: "HTML to my lib",
-    description: "convert html to js format",
-    link: "html-to-my-lib/",
-    routeFunc: () => {
-        const fspage = HtmlToMyLib();
-        page.mainBody.clear();
-        page.mainBody.display(fspage);
-        page.layout.s.wrapper.s.header.s.updateTitle(
-            DEF_TITLE + " - " + "HTML to my lib"
-        );
-    },
-    icon: CircleCheck,
-});
-page.router.route();
 document.querySelector<HTMLDivElement>("#app")!.appendChild(page.getElement());
