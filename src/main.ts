3d257283909a15913c939f76b74a2e9347c4faed
Raja <rajababuchauhan500@gmail.com>
2026-02-06 18:07:16 +0100

    remove git search app

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { Page } from "./rlib/timeline/t2025/may/ToolsHomepage/Page";
 import "./style.css";
 import { CircleCheck } from "lucide";
-import { Page as FileSearchPage } from "./rlib/timeline/t2025/may/FileSearch/Components";
 import { Page as HtmlToMyLib } from "./rlib/timeline/t2025/may/HtmlToMyLib/index";
 
 import { DEF_TITLE } from "./rlib/timeline/t2025/may/ToolsHomepage/Components";
@@ -11,35 +10,6 @@ page.layout.s.wrapper.s.header.s.wrapper.s.goBack.s.img.update({
     src: "logo.png",
 });
 
-page.addApp({
-    title: "Content Searching",
-    description: "Search in your git repo files content",
-    link: "task-manager/",
-    routeFunc: () => {
-        const fspage = FileSearchPage();
-        page.mainBody.clear();
-        page.mainBody.display(fspage);
-        page.layout.s.wrapper.s.header.s.updateTitle(
-            DEF_TITLE + " - " + "Content Searching"
-        );
-    },
-    icon: CircleCheck,
-});
-
-page.addApp({
-    title: "Links Opener",
-    description: "Search in your git repo files content",
-    link: "task-manager/",
-    routeFunc: () => {
-        const fspage = FileSearchPage();
-        page.mainBody.clear();
-        page.mainBody.display(fspage);
-        page.layout.s.wrapper.s.header.s.updateTitle(
-            DEF_TITLE + " - " + "Content Searching"
-        );
-    },
-    icon: CircleCheck,
-});
 
 page.addApp({
     title: "HTML to my lib",
--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,14 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
     plugins: [
         tailwindcss(),
-        nodePolyfills({
-            globals: {
-                Buffer: true,
-                global: true,
-                process: true,
-            },
-            protocolImports: true,
-        }),
-        viteStaticCopy({
-            targets: [
-                {
-                    src: "node_modules/ace-builds/src-noconflict",
-                    dest: "assets",
-                },
-            ],
-        }),
     ],
+    resolve: {
+        alias: {
+            'unenv/node/buffer': 'unenv/runtime/node/buffer/index',
+            'unenv/node/process': 'unenv/runtime/node/process/index',
+        },
+    },
 });
